ddeaf3fa0510fd7d6ed51717bb59188c666d592e
Pouriya_tabari <pouriya_tabari@outlook.com>
2023-02-07 08:17:19 +0330

    adding comment

---
+++ shark.sh
@@ -0,0 +1,15 @@
+#-------------------1- Check OS---------------------------------------------
+#-------------------2- Change or check in SSH config file-------------------
+#-------------------2-a) Change or check default port-----------------------
+#-------------------2-b) Change or check disable password authentication----
+#-------------------2-c) Change or check enable pub_key authentication------
+#-------------------2-d) Ensure that .ssh/authorized_key is exists----------
+#-------------------2-e) Create SSH Group For AllowGroup--------------------
+#-------------------2-f) Remove Short Diffie-Hellman Keys-------------------
+#-------------------2-g) 2FA/MFA for SSH------------------------------------
+#-------------------3- Check NTP Client-------------------------------------
+#-------------------4- Check Fail2ban is installed or no--------------------
+#-------------------5- Check Firewall is running or no----------------------
+#-------------------6- Gathering ports you want to allow or use-------------
+#-------------------7- Limit for sudo---------------------------------------
+#-------------------8- Limit for su-----------------------------------------
